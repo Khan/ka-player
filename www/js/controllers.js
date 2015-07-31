9a56c26fd858547b34909b1a98435efdaa1930ba
Neel Mehta <neelmehta@college.harvard.edu>
2015-07-31 13:18:06 -0700

    Remove dead code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,45 +2,8 @@ angular.module("starter.controllers", [])
 .config(function ($sceDelegateProvider) {
     $sceDelegateProvider.resourceUrlWhitelist(['self', '*khanacademy.org*', 'http://www.khanacademy.org/', 'https://www.khanacademy.org/', 'http://khanacademy.org/', 'https://khanacademy.org/']);
 })
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // // Form data for the login modal
-  // $scope.loginData = {};
-  //
-  // // Create the login modal that we will use later
-  // $ionicModal.fromTemplateUrl('templates/login.html', {
-  //   scope: $scope
-  // }).then(function(modal) {
-  //   $scope.modal = modal;
-  // });
-  //
-  // // Triggered in the login modal to close it
-  // $scope.closeLogin = function() {
-  //   $scope.modal.hide();
-  // };
-  //
-  // // Open the login modal
-  // $scope.login = function() {
-  //   $scope.modal.show();
-  // };
-  //
-  // // Perform the login action when the user submits the login form
-  // $scope.doLogin = function() {
-  //   console.log('Doing login', $scope.loginData);
-  //
-  //   // Simulate a login delay. Remove this and replace with your login
-  //   // code if using a login system
-  //   $timeout(function() {
-  //     $scope.closeLogin();
-  //   }, 1000);
-  // };
 })
 
 
@@ -437,15 +400,6 @@ angular.module("starter.controllers", [])
             program.favorite = true;
         });
     });
-  //
-  // // for testing
-  // _.delay(function(){
-  //     programFactory.createProgramFromId(5536800924893184).then(function(program){
-  //         console.log("Adding", program.title);
-  //         service.addProgramById(program);
-  //         program.favorite = true;
-  //     });
-  // }, 5000);
 
   return service;
 })
