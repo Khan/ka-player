d7530ad86d063fe989de6066be5aafdbfd1eb538
Neel Mehta <neelmehta@college.harvard.edu>
2015-08-01 13:35:06 -0700

    Beautify app.js

--- www/js/app.js
+++ www/js/app.js
@@ -7,95 +7,93 @@
 angular.module('ka-player', ['ionic', 'ka-player.controllers'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 .config(function($stateProvider, $urlRouterProvider, $sceDelegateProvider) {
-  $stateProvider
+    $stateProvider
+        .state('app', {
+            url: '/app',
+            abstract: true,
+            templateUrl: 'templates/menu.html',
+            controller: 'AppCtrl'
+        })
+        .state('app.add', {
+            url: '/add',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/add.html',
+                    controller: 'AddCtrl'
+                }
+            }
+        })
+        .state('app.favorites', {
+            url: '/favorites',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/favorites.html',
+                    controller: 'FavoritesCtrl'
+                }
+            }
+        })
+        .state('app.browse', {
+            url: '/browse',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/browse.html',
+                    controller: 'BrowseCtrl'
+                }
+            }
+        })
+        .state('app.search', {
+            url: '/search',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/search.html',
+                    controller: 'SearchCtrl'
+                }
+            }
+        })
+        .state('app.makeYourOwn', {
+            url: '/makeYourOwn',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/makeYourOwn.html',
+                    controller: 'MakeYourOwnCtrl'
+                }
+            }
+        })
+        .state('app.player', {
+            url: '/play/:programId',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/player.html',
+                    controller: 'PlayerCtrl'
+                }
+            }
+        });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/favorites');
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.add', {
-      url: '/add',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/add.html',
-          controller: 'AddCtrl'
-        }
-      }
-    })
-    .state('app.favorites', {
-      url: '/favorites',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/favorites.html',
-          controller: 'FavoritesCtrl'
-        }
-      }
-    })
-    .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html',
-          controller: 'BrowseCtrl'
-        }
-      }
-    })
-    .state('app.search', {
-      url: '/search',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/search.html',
-          controller: 'SearchCtrl'
-        }
-      }
-    })
-    .state('app.makeYourOwn', {
-      url: '/makeYourOwn',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/makeYourOwn.html',
-          controller: 'MakeYourOwnCtrl'
-        }
-      }
-    })
-    .state('app.player', {
-      url: '/play/:programId',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/player.html',
-          controller: 'PlayerCtrl'
-        }
-      }
-    });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/favorites');
-
-  // whitelist certain domains so we're allowed to render iframes from those
-  $sceDelegateProvider.resourceUrlWhitelist([
-      'self',
-      '*khanacademy.org*',
-      'http://www.khanacademy.org/',
-      'https://www.khanacademy.org/',
-      'http://khanacademy.org/',
-      'https://khanacademy.org/']
-  );
+    // whitelist certain domains so we're allowed to render iframes from those
+    $sceDelegateProvider.resourceUrlWhitelist([
+        'self',
+        '*khanacademy.org*',
+        'http://www.khanacademy.org/',
+        'https://www.khanacademy.org/',
+        'http://khanacademy.org/',
+        'https://khanacademy.org/'
+    ]);
 });
